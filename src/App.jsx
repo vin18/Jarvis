95d9d063a8d82b68aa8666c6538c71151a29fe37
Vinit Raut <vinit.coder@gmail.com>
2023-07-26 21:45:25 +0530

    Add: tanstack query for server side state management, api calling and caching

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import AppLayout from './components/AppLayout';
 import { Button } from './components/ui/button';
@@ -7,19 +9,24 @@ import Employees from './pages/Employees';
 import Leaves from './pages/Leaves';
 import Login from './pages/Login';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<AppLayout />}>
-          <Route index element={<Navigate replace to="dashboard" />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/leaves" element={<Leaves />} />
-          <Route path="/employees" element={<Employees />} />
-        </Route>
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/leaves" element={<Leaves />} />
+            <Route path="/employees" element={<Employees />} />
+          </Route>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
