6525a81cb050fc99e04786d555df3fc0d060dfc9
Vinit Raut <vinit.coder@gmail.com>
2023-09-07 11:45:53 +0530

    Add: WIP for components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { Toaster } from 'react-hot-toast';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { Toaster } from 'react-hot-toast'
 
-import AppLayout from './components/AppLayout';
-import Dashboard from './pages/Dashboard';
-import Employees from './pages/Employees';
-import Leaves from './pages/Leaves';
-import Login from './pages/Login';
-import ProtectedPage from './pages/ProtectedPage';
-import Users from './pages/Users';
+import AppLayout from './components/AppLayout'
+import Dashboard from './pages/Dashboard'
+import Employees from './pages/Employees'
+import Leaves from './pages/Leaves'
+import Login from './pages/Login'
+import ProtectedPage from './pages/ProtectedPage'
+import Users from './pages/Users'
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient()
 
 function App() {
   return (
@@ -25,7 +25,7 @@ function App() {
               </ProtectedPage>
             }
           >
-            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route index element={<Navigate replace to="employees" />} />
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/leaves" element={<Leaves />} />
             <Route path="/employees" element={<Employees />} />
@@ -37,7 +37,7 @@ function App() {
       <ReactQueryDevtools initialIsOpen={false} />
       <Toaster position="top-right" gutter={12} />
     </QueryClientProvider>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/pages/Leaves.jsx
+++ src/pages/Leaves.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
+import React from 'react'
 
 export default function Leaves() {
-  return <div>Leaves</div>;
+  return <div>Leaves WIP</div>
 }
